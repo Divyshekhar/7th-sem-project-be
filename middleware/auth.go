49acab9e418dc8ca27f08746493c77b8ed2cb4ff
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2025-07-24 14:59:59 +0530

    feat: auth middleware

---
+++ middleware/auth.go
@@ -0,0 +1,38 @@
+package middleware
+
+import (
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func RequireAuth() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		tokenStr, err := ctx.Cookie("jwt_token")
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "no token found"})
+			return
+		}
+		token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("SECRET")), nil
+		})
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+			return
+		}
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok || !token.Valid {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+			return
+		}
+		if exp, ok := claims["exp"].(float64); !ok || float64(time.Now().Unix()) > exp {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "token expired"})
+			return
+		}
+		userId := claims["user_id"]
+		ctx.Set("user_id", userId)
+	}
+}
