c352379e509b071538ccd968079de633fc81cca8
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2025-07-25 22:29:26 +0530

    fix: update comments in streaming

--- googleai.go
+++ googleai.go
@@ -17,10 +17,14 @@ func callGemini() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// for streaming messages
+
 	content := []llms.MessageContent{
 		llms.TextParts(llms.ChatMessageTypeSystem, "You are a company branding design wizard."),
 		llms.TextParts(llms.ChatMessageTypeHuman, "What would be a good company name for a comapny that produces Go-backed LLM tools?"),
 	}
+
 	completion, err := llm.GenerateContent(ctx, content, llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
 		fmt.Print(string(chunk))
 		return nil
